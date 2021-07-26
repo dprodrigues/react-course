87ba907a29bbdac32997df74849c52456a2831d1
dprodrigues <dandan.dpr@gmail.com>
2021-07-26 19:53:00 -0300

    feat: add files from github's project

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,30 @@
+import P from "prop-types";
+
+import "./styles.css";
+
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts = [] }) => (
+	<div className="posts">
+		{posts.map((post) => (
+			<PostCard
+				body={post.body}
+				cover={post.cover}
+				id={post.id}
+				key={post.id}
+				title={post.title}
+			/>
+		))}
+	</div>
+);
+
+Posts.propTypes = {
+	posts: P.arrayOf(
+		P.shape({
+			body: P.string.isRequired,
+			cover: P.string.isRequired,
+			id: P.number.isRequired,
+			title: P.string.isRequired
+		}),
+	),
+};
